8022a14f3709f566a297f1bc5bf11f955aae863a
Bluryyy <wallexfernandss@gmail.com>
2020-12-15 18:35:23 -0300

    Selecionar e Alterar um aviso

--- components/notice/notice.js
+++ components/notice/notice.js
@@ -23,6 +23,28 @@ async function salvar(aviso) {
 }//fim do salvar
 
 /**
+ * Alterar um aviso cadastrado
+ * @param {object} aviso O aviso deve estar no seguinte formato: {titulo: <string>, data: <string>, mensagem: <string>}
+ * @param {int} id ID do aviso
+ * @returns {object} Mensagem de sucesso ou erro
+ */
+function editar(aviso, id){
+  return db('avisos').where('ID_avisos', id).update(aviso)
+  .then(_ => {
+    return {
+      tipo: "Sucesso",
+      corpo: "Aviso alterado com sucesso!"
+    }
+  })
+    .catch(erro => {
+      return {
+        tipo: "Erro",
+        corpo: "Erro ao Alterar" + erro
+      }
+    })
+}//fim do alterar
+
+/**
  * Seleciona todos os avisos cadastrados
  * @returns {object} Objeto com todos os avisos cadastrados ou uma mensagem de erro
  */
@@ -37,6 +59,20 @@ function selecionarTodos(){
 }//fim do selecionar
 
 /**
+ * Selecionar um aviso
+ * @param {*} id ID do aviso que será selecionado 
+ * @returns {object} Objeto com o aviso selecionado
+ */
+function selecionarAviso(id){
+  return db.select('*').from('avisos').where('ID_avisos', id).first()
+  .then (aviso => { return aviso })
+  .catch (erro => {
+    return { tipo:"erro", corpo:"Erro: " + erro }
+  })
+}//fim do selecionar aviso
+
+
+/**
  * Função que excluir um aviso do banco de dados
  * @param {int} id Id dos avisos
  */
@@ -44,4 +80,4 @@ function excluir (id){
   return db.del().from('avisos').where('ID_avisos',id)
 }
 
-module.exports = {salvar, selecionarTodos, excluir}
+module.exports = {salvar, selecionarTodos, selecionarAviso, excluir, editar}
--- components/notice/noticeController.js
+++ components/notice/noticeController.js
@@ -17,6 +17,12 @@ routes.get("/notice/new",(req,res)=>{
   res.render('forms')
 })//Fim da Route
 
+routes.get("/notice/editar/:id", async (req, res)=> {
+  const id = req.params.id
+  const aviso = await notice.selecionarAviso(id)
+  res.render('forms',{aviso})
+})
+
 routes.post("/notice/new", async (req,res)=>{
   const titulo = req.body.titulo
   const data = req.body.data
@@ -25,6 +31,21 @@ routes.post("/notice/new", async (req,res)=>{
   const msg = await notice.salvar({titulo,data,mensagem})
 
  res.render('forms',{msg})
+})
+
+routes.post("/notice/editar/:id", async (req, res)=> {
+  const id = req.body.id
+  const titulo = req.body.titulo
+  const data = req.body.data
+  const mensagem = req.body.mensagem
+
+  const msg = await notice.editar({titulo,data,mensagem}, id)
+
+  if(msg.tipo === "Sucesso"){
+    res.redirect('/notice')
+  }else{
+    res.render('forms',{msg})
+  }
 })
 
 routes.get("/notices/excluir/:id", async (req,res)=>{
